4caea8acd0096700f94f8fe1ed9c5754cb95bc1c
Batbaatar Tuya <tbatbaatar@gmail.com>
2023-11-20 15:13:12 +0800

    Updates

---
+++ precache-manifest.8cee45f6320004da014ad486e7136c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388ba32ce3923b5912df50cd3e696750",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "756fa6ae94bcd3dc312c",
+    "url": "/burger/static/css/main.20acc057.chunk.css"
+  },
+  {
+    "revision": "50fb320d5b17571a3b33",
+    "url": "/burger/static/js/2.b1f029f9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.b1f029f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756fa6ae94bcd3dc312c",
+    "url": "/burger/static/js/main.25c767fb.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
